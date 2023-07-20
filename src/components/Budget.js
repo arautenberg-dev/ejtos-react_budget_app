91a6e78e771d799f01e0f36a7fc04ccb149d55d4
Alexander Rautenberg <dev@rautenberg.ch>
2023-07-20 15:21:52 -0400

    in progress

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,28 @@
 
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
     const { budget } = useContext(AppContext);
+    //const [budget, setBudget] = useState('');
+
+    const setBudget = (val) =>
+    {
+        if(val > 20000)
+            alert('the value cannot be > 20000');
+    }
+
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: 
+            <input
+                        required='required'
+                        type='number'
+                        id='budget'
+                        value='20000'
+                        //style={{ marginLeft: '1rem' , size: 7}}
+                        onChange={(event) => setBudget(event.target.value)}>
+                        </input>
+                        </span>
         </div>
     );
 };
